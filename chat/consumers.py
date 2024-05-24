6dc486132133097587145862546bdeb74e5dd282
nandu44-code <nanduganeshnair4444@gmail.com>
2024-05-24 11:50:33 +0530

    created Message model and consumers

---
+++ chat/consumers.py
@@ -0,0 +1,35 @@
+# chat/consumers.py
+
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Save message to database
+        Message.objects.create(user=self.scope['user'], content=message)
+
+        # Send message to all clients in the same room
+        await self.channel_layer.group_send(
+            'chat_room',
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from user.models import CustomUser
 # Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
+
+
