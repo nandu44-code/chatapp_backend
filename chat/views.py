ef77266ce6094fe5b54d348dd016e78ebee4e785
nandu44-code <nanduganeshnair4444@gmail.com>
2024-05-24 11:56:51 +0530

    created serializers,routing and views file

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Message
+from .serializers import MessageSerializer
 
-# Create your views here.
+class MessageListCreate(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
